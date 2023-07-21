705f40d03568aebf0d9d92670e7820504ca8aed5
Rudis <82242977+krakatua@users.noreply.github.com>
2023-07-21 19:54:11 -0400

    values changed

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,19 +1,23 @@
 import { TodoId, type Todo as TodoType } from "./Types";
 
 interface Props extends TodoType {
-    onRemoveTodo: (id: TodoId) => void;
+    onRemoveTodo: ({id}: TodoId) => void;
     onToggleCompleteTodo: ({id, completed}: Pick<TodoType, 'id' | 'completed'>) => void
 }
 export const Todo: React.FC<Props> = ({ id, title, completed, onRemoveTodo, onToggleCompleteTodo }) => {
+    const handleChangeCheckbox = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        onToggleCompleteTodo({
+            id,
+            completed: event.target.checked
+        })
+    }
     return (
         <div className="view">
             <input 
             className="toggle" 
             type="checkbox" 
             checked={completed} 
-            onChange={(event) => { 
-                onToggleCompleteTodo({id, completed: event.target.checked})
-            }} 
+            onChange={handleChangeCheckbox} 
             />
             <label>{title}</label>
             <button
